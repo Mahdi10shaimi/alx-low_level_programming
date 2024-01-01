5dc4ebbc4c3fe9c9b0075e7acdad2b1a2959d637
Mahdi10shaimi <elmahdishaimi@gmail.com>
2024-01-01 06:21:15 -0800

    i

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		if (i == 'e' || i == 'q')
+		{
+			i++;
+		}
+		else
+			putchar(i);
+		i++;
+	}
+	putchar ('\n');
+	return (0);
+}
