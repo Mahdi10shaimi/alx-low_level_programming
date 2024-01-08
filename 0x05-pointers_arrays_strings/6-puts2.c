db9b7a9a27d359ffd46507d8331bc6cfecc7e0f2
Mahdi10shaimi <elmahdishaimi@gmail.com>
2024-01-08 12:09:23 -0800

    k

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts2 -  function that prints every other character of a string
+ * @str : parametre
+ * Return : void
+ */
+void puts2(char *str)
+{
+	int i, len, j;
+	i = 0;
+	j = 0;
+	len = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	len = i - 1;
+	while (j <= len)
+	{
+		if (j % 2 != 0)
+		{
+			j++;
+		}
+		else 
+		{
+			_putchar(str[j]);
+			j++;
+		}
+	}
+	_putchar('\n');
+}
