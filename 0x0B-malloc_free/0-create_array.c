cab11d7dd5d5fb6ddb3731275e8773dd7fc82ec4
Mahdi10shaimi <elmahdishaimi@gmail.com>
2024-01-20 04:21:38 -0800

    j

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_array - function that creates an array of chars
+ * @size : parametre
+ * @c : parametre
+ * Return: pointer or null
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+	{
+		printf("insufficient memory\n");
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			arr[i] = c;
+		}
+	}
+	return (arr);
+}
