119da430372fb32170d41160479218594fa51642
Mahdi10shaimi <elmahdishaimi@gmail.com>
2024-02-06 10:17:53 -0800

    K

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,4 +30,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (idx == 0)
 		{
 			new_node->next = *head;
-
+			*head = new_node;
+			return (new_node);
+		}
+		if (count + 1 == idx)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
+		}
+	}
+	free(new_node);
+	return (NULL);
+}
